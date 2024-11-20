6dd0c9ba20954edfe55c6d4d2a3c44590d39f606
Andrea Manzini <ilmanzo@gmail.com>
2024-11-20 11:46:35 +0100

    fixed bugs on maze game

--- a_maze/game/internal/maze.go
+++ a_maze/game/internal/maze.go
@@ -1,12 +1,16 @@
 package maze
 
 import (
-	"strings"
-
 	"math/rand"
+	"strings"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	fps = 25
 )
 
 // this is our data model
@@ -31,13 +35,13 @@ var treasureStyle = lipgloss.NewStyle().Background(commonBG).Foreground(lipgloss
 
 // 0 = empty space
 // 1 = wall
-// 2 = player
-// 3 = treasure
+// 2 = treasure
+// 3 = player
 var valToString = [4]string{
 	mazeStyle.Render("  "),
 	mazeStyle.Render("\u2588\u2588"),
-	playerStyle.Render("🦔"),
 	treasureStyle.Render("🎂"),
+	playerStyle.Render("🦔"),
 }
 
 func NewMaze(w, h int) maze {
@@ -82,10 +86,10 @@ func NewMaze(w, h int) maze {
 	//and treasure (random)
 	m.playerX = w / 2
 	m.playerY = h / 2
-	m.set(m.playerX, m.playerY, 2)
-	m.treasureX = 2 + rand.Intn(w-3)
-	m.treasureY = 2 + rand.Intn(h-3)
-	m.set(m.treasureX, m.treasureY, 3)
+	m.set(m.playerX, m.playerY, 3)
+	m.treasureX = 3 + rand.Intn(w-4)
+	m.treasureY = 3 + rand.Intn(h-4)
+	m.set(m.treasureX, m.treasureY, 2)
 	return m
 }
 
@@ -97,6 +101,21 @@ func (m *maze) set(x, y int, value byte) {
 		return
 	}
 	m.cells[i] = value
+}
+
+func (m *maze) get(x, y int) byte {
+	i := y*m.width + x
+	if i > len(m.cells)-1 || x < 0 || y < 0 || x >= m.width || y >= m.height {
+		return 1
+	}
+	return m.cells[i]
+}
+
+func (m *maze) checkForTreasure() (tea.Model, tea.Cmd) {
+	if m.playerX == m.treasureX && m.playerY == m.treasureY {
+		return m, tea.Quit
+	}
+	return m, nil
 }
 
 // returns a string representing our model
@@ -116,7 +135,15 @@ func (m maze) View() string {
 
 // nothing to do on startup
 func (m maze) Init() tea.Cmd {
-	return nil
+	return animate()
+}
+
+type frameMsg struct{}
+
+func animate() tea.Cmd {
+	return tea.Tick(time.Second/fps, func(_ time.Time) tea.Msg {
+		return frameMsg{}
+	})
 }
 
 // this function is automatically called by framework
@@ -124,10 +151,47 @@ func (m maze) Init() tea.Cmd {
 func (m maze) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		return m, tea.Quit
+		if msg.Type == tea.KeyCtrlC || msg.Type == tea.KeyEsc {
+			return m, tea.Quit
+		}
+		if msg.Type == tea.KeyUp {
+			if m.get(m.playerX, m.playerY-1)%2 == 0 {
+				m.set(m.playerX, m.playerY, 0)
+				m.playerY -= 1
+				m.set(m.playerX, m.playerY, 3)
+				return m.checkForTreasure()
+			}
+		}
+		if msg.Type == tea.KeyDown {
+			if m.get(m.playerX, m.playerY+1)%2 == 0 {
+				m.set(m.playerX, m.playerY, 0)
+				m.playerY += 1
+				m.set(m.playerX, m.playerY, 3)
+				return m.checkForTreasure()
+			}
+		}
+		if msg.Type == tea.KeyLeft {
+			if m.get(m.playerX-1, m.playerY)%2 == 0 {
+				m.set(m.playerX, m.playerY, 0)
+				m.playerX -= 1
+				m.set(m.playerX, m.playerY, 3)
+				return m.checkForTreasure()
+			}
+		}
+		if msg.Type == tea.KeyRight {
+			if m.get(m.playerX+1, m.playerY)%2 == 0 {
+				m.set(m.playerX, m.playerY, 0)
+				m.playerX += 1
+				m.set(m.playerX, m.playerY, 3)
+				return m.checkForTreasure()
+			}
+		}
+	case frameMsg:
+		return m, animate()
 	case tea.WindowSizeMsg:
 		return NewMaze(msg.Width/2, msg.Height), nil
 	default:
 		return m, nil
 	}
+	return m, nil
 }
