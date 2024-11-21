cb868a722e5171f2b11a6402ea8e3efd9d8f4aa2
Andrea Manzini <ilmanzo@gmail.com>
2024-11-21 09:37:55 +0100

    toot.py : make linter happy

--- train_of_though/toot.py
+++ train_of_though/toot.py
@@ -8,60 +8,55 @@ import textwrap
 
 
 def random_color():
+    "returns a random foreground ANSI color with bold attribute set"
     fg = randrange(31, 39)
     return f"\x1b[1;{fg}m"
 
 
 def has_fortune():
+    "detects when fortune program is installed"
     return which('fortune') is not None
 
 
 def get_fortune_quote():
+    "run external program fortune, and formats the output to fit in 48 char width"
     result = subprocess.check_output('fortune')
     return textwrap.wrap(result.decode('utf-8'), width=48)
 
 
 def insert_string_fixed_length(original_string, insert_string):
     """Inserts a string into another string, keeping the total length the same.
-  
     Args:
       original_string: The string to insert into.
       insert_string: The string to be inserted.
-  
     Returns:
       A new string with the inserted string, or None if the insertion is not possible.
     """
     if len(insert_string) > len(original_string):
         return None  # Cannot insert if the new string is longer
-  
     # Calculate the middle of the string
     midpoint = len(original_string) // 2
-  
     # Calculate the starting position for the insert string
     start_pos = 9+midpoint - len(insert_string) // 2
-  
     # Calculate how many characters need to be removed from the original string
     chars_to_remove = len(insert_string)
-  
     # Construct the new string
     new_string = (
         original_string[: start_pos]
         + insert_string
         + original_string[(start_pos + chars_to_remove):]
     )
-  
     return new_string
-
 
 
 COLOR_RESET = "\x1b[0m"
 
 if not has_fortune():
-  print("Sorry you need to install the fortune package!")
-  print("$ sudo zypper install fortune")
-  exit(1)
+    print("Sorry you need to install the fortune package!")
+    print("$ sudo zypper install fortune")
+    exit(1)
 
-
+# ASCII train art from https://www.asciiart.eu/vehicles/trains
 train = [
   "ToOT : THE Train OOF Thought - SUSE HackWeek 2024",
   "                 _-====-__-======-__-========-_____-============-__",
@@ -78,18 +73,17 @@ train = [
   "#####################################################################",
   "Train art by Donovan Bake"]
 
+# only accepts 'short' quotes that fits in the train bubble
 while True:
     quotes = get_fortune_quote()
     if len(quotes) < 5:
         break
-    
+
 for i, q in enumerate(quotes):
     train[i+2] = insert_string_fixed_length(train[i+2], q)
-    
 
 # run fortune program and fits the output inside a 4x48 = 192 chars
 
 print(random_color())
 print("\n".join(train))
-
 print(COLOR_RESET)
